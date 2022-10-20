48ec28a7c6f325e9de03550e82ad9f6030421e62
Gabriel Sanchez <gabriel.sanchez4294@alumnos.udg.mx>
2022-10-20 12:01:04 -0500

    6 caracteres registro usuario

--- src/components/Auth/RegisterForm/RegisterForm.data.js
+++ src/components/Auth/RegisterForm/RegisterForm.data.js
@@ -11,7 +11,7 @@ export function initialValues() {
 export function validationSchema() {
     return Yup.object({
         email: Yup.string().email("El email no es válido").required("El email es obligatorio"),
-        password: Yup.string().required("La contraseña es obligatoria"),
-        repeatPassword: Yup.string().required("La contraseña es obligatoria").oneOf([Yup.ref("password")],"Las contraseñas tienen que ser iguales"),
+        password: Yup.string().min(6, 'Mínimo 6 caracteres').required("La contraseña es obligatoria"),
+        repeatPassword: Yup.string().min(6, 'Mínimo 6 caracteres').required("La contraseña es obligatoria").oneOf([Yup.ref("password")],"Las contraseñas tienen que ser iguales"),
     });
 }
