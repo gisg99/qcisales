ed59f7c944874d801c8bef772d1ed946d6b1abca
Gabriel Sanchez <gabriel.sanchez4294@alumnos.udg.mx>
2022-10-24 16:33:39 -0500

    Agregado el botón para contactar al vendedor por WhatsApp

--- src/components/Seller/Info/Info.js
+++ src/components/Seller/Info/Info.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { View } from "react-native";
-import { Text, ListItem, Icon} from "react-native-elements";
+import { View, Linking } from "react-native";
+import { Text, ListItem, Icon, Button} from "react-native-elements";
 import { map } from "lodash";
 import { styles } from "./Info.styles";
 
@@ -30,6 +30,20 @@ export function Info(props) {
             </ListItem.Content>
         </ListItem>
       ))}
+      <ListItem
+        key={"1"}
+        bottomDivider
+        onPress={ () => Linking.openURL(`https://wa.me/52${seller.phone}?text=¡Hola!%20Vi%20tu%20publicacion%20en%20qci%20sales%20y%20quisiera%20saber%20si%20aun%20tienes%20${seller.name}`)}
+        >
+            <Icon
+            type="material-community"
+            name="whatsapp"
+            color="#00A680"
+                />
+            <ListItem.Content>
+                <ListItem.Title>Enviar mensaje al vendedor</ListItem.Title>
+            </ListItem.Content>
+      </ListItem>
     </View>
   )
 }
