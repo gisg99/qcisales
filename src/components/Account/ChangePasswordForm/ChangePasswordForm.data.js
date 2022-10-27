5c36f52d09f0fdfcdd21af83a95862722ec5a4b3
Gabriel Sanchez <gabriel.sanchez4294@alumnos.udg.mx>
2022-10-27 11:35:26 -0500

    Corrección en la validación del formulario de cambio de contraseña

--- src/components/Account/ChangePasswordForm/ChangePasswordForm.data.js
+++ src/components/Account/ChangePasswordForm/ChangePasswordForm.data.js
@@ -11,7 +11,7 @@ export function initialValues() {
 export function validationSchema() {
     return Yup.object({
         password: Yup.string().required("La contraseña actual es obligatoria"),
-        newPassword: Yup.string().min(20, 'Mínimo 6 carácteres').max(20, 'Máximo 20 carácteres').required("La nueva contraseña es obligatoria"),
-        newPasswordC: Yup.string().min(20, 'Mínimo 6 carácteres').max(20, 'Máximo 20 carácteres').required("La confirmación de contraseña es obligatoria").oneOf([Yup.ref("newPassword")],"Las contraseñas nuevas tienen que ser iguales"),
+        newPassword: Yup.string().min(6, 'Mínimo 6 carácteres').max(20, 'Máximo 20 carácteres').required("La nueva contraseña es obligatoria"),
+        newPasswordC: Yup.string().min(6, 'Mínimo 6 carácteres').max(20, 'Máximo 20 carácteres').required("La confirmación de contraseña es obligatoria").oneOf([Yup.ref("newPassword")],"Las contraseñas nuevas tienen que ser iguales"),
     });
 }
